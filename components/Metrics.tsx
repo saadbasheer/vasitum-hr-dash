9a9a93d1d29cd35322ccac1ad77fea61bd15b47d
saadbasheer <saadbasheer0@gmail.com>
2024-09-24 13:53:11 +0530

    first commit

--- components/Metrics.tsx
+++ components/Metrics.tsx
@@ -48,16 +48,23 @@ function DashboardCard({ data }: { data: CardData }) {
       <div className="flex p-7 space-x-3 justify-evenly">
         <div className=" gap-10 ">
           <div className="mb-5 space-y-7">
-            <div className="title-medium text-[#161E54]">{title}</div>
-            <div className="metric-value text-[#161E54]">{value}</div>
+            <div className="title-medium  text-[#161E54]">{title}</div>
+            <div className="w-[166px] h-[28px] font-poppins text-5xl font-medium leading-7 text-left text-[#161E54]">
+              {value}
+            </div>
           </div>
           <div className="metric-breakdown text-[#686868]">
             <p>120 men</p> <p>96 women</p>
           </div>
         </div>
         <div className=" space-y-5 ">
-          <div className="metric-trend"> <Trend/></div>
-          <div className="text-center rounded-lg p-1 bg-[#FFEFE7]">+2% last month</div>
+          <div className="metric-trend">
+            {" "}
+            <Trend />
+          </div>
+          <div className="text-center rounded-lg p-1 bg-[#FFEFE7]">
+            +2% last month
+          </div>
         </div>
       </div>
     </div>
